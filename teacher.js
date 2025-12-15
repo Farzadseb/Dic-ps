058e05f13b9db29a49cdec95f73ebd28250535ee
Farzadseb <farzad.seb@gmail.com>
2025-12-15 23:47:50 +0330

    Create teacher.js

---
+++ teacher.js
@@ -0,0 +1,11 @@
+// فایل منطق معلم - teacher.js
+
+// بارگذاری پنل معلم
+function initTeacherPanel() {
+    loadTeacherStats();
+    loadCodesList();
+    loadStudentsList();
+    loadSettings();
+}
+
+// بقیه توابع مدیریت معلم...
